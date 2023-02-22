81cd856dabe69e70481d570ec635acadd378cce0
jsungl <wotjd931026@gmail.com>
2023-02-23 04:47:29 +0900

    Feat: sequelize 모델 정의

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development'; // 지정된 환경변수가 없으면 'development'로 지정
+// 클래스를 불러온다.
+const User = require('./user');
+const Comment = require('./comment');
 /**
  * config/config.json 파일에 있는 설정값들을 불러온다.
  * config객체의 env변수(development)키 의 객체값들을 불러온다.
@@ -14,5 +17,18 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 
 // 연결객체를 나중에 재사용하기 위해 db.sequelize에 넣어둔다.
 db.sequelize = sequelize;
+
+// 모델 클래스를 넣음.
+db.User = User;
+db.Comment = Comment;
+
+// 모델과 테이블 종합적인 연결이 설정된다.
+User.init(sequelize); 
+Comment.init(sequelize);
+
+// db객체 안에 있는 모델들 간의 관계가 설정된다.
+User.associate(db);
+Comment.associate(db);
+
 // 모듈로 꺼낸다.
 module.exports = db;
