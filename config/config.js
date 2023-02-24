d7efdf058b4f78521b09dc26ee5991d487ed31ef
jsungl <wotjd931026@gmail.com>
2023-02-25 03:40:49 +0900

    Feat: sequelize 로그 끄기

--- config/config.js
+++ config/config.js
@@ -1,6 +1,33 @@
 require('dotenv').config();
 
+module.exports = {
 
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false // 배포 환경에서는 쿼리 로그를 비활성화 한다.
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  }
+};
+
+/*
 const development = {
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
@@ -22,7 +49,9 @@ const production = {
   "password": null,
   "database": "database_production",
   "host": "127.0.0.1",
-  "dialect": "mysql"
+  "dialect": "mysql",
 };
 
 module.exports = { development, production, test };
+
+*/
